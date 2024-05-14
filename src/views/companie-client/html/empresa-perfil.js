a8fa0e96e013bd0289f43b67232bf86675252692
AnaS <ana.sarmiento.2022@upb.edu.co>
2024-05-14 13:37:11 -0500

    cambio de TODO

--- src/views/companie-client/html/empresa-perfil.js
+++ src/views/companie-client/html/empresa-perfil.js
@@ -327,7 +327,9 @@ const EmpresaPerfil = (props) => {
                   <textarea
                     type="text"
                     id="input_info"
-                    className="contenedores-de-modales-empresa-perfil-textinput6 input"
+                    placeholder='Descripción'
+                    
+                    className="textarea"
                     defaultValue={infoCompanie.descripcion_publica}
                   />
                 </div>
